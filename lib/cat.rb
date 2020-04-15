514a41d8ecc2b54bbbe95c4b605b336a78603bd4
John Zonneveld <jjczon@gmail.com>
2020-04-15 22:58:29 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
+require 'pry'
 class Cat
-  # code goes here
+  attr_reader :name, :owner
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    binding.pry
+  end
+
+  # def owner(new_owner)
+  #   self.owner = new_owner
+  # end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,33 @@
+
 class Owner
-  # code goes here
+  attr_reader :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+    species
+    pets = []
+  end
+
+  def species
+    @species = "human"
+  end
+
+  def say_species
+    ## binding.pry
+    return "I am a #{@species}."
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
 end
